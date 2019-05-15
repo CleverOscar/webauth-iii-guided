09e474d7a5862eb377c487209f9f3812b2c655d6
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-05-15 15:29:18 -0500

    Finished MVP

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,26 @@
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  // tokens are commonly sent as the authorization header
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if(token){
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err){
+        //the token is not valid
+        res.status(401).json({ message: "Forbidden!"});
+      } else {
+        // the token is valid
+        req.decodedJwt = decodedToken; // make it available to the rest of our API
+        console.log('decoded token', req.decodedJwt);
+
+        next();
+      }
+    })
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    // no token ? bounced out!
+    res.status(401).json({ message: "You shall not pass!"})
   }
 };
